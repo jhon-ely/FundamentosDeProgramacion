a21ed617797209faeb490f4035380e9f98a2c07e
Jhon Sebastian Suarez Perez <77938466+jhon-ely@users.noreply.github.com>
2021-01-24 21:56:22 -0500

    Update hipotenusa t angulo a - copia.cs

--- hipotenusa t angulo a - copia.cs
+++ hipotenusa t angulo a - copia.cs
@@ -13,11 +13,11 @@ namespace hipotenusa_t_y_angulo_a
             Console.WriteLine("escriba el angulo a");
             double a = double.Parse(Console.ReadLine());
             //encontrar cateto y
-            double agra = (a * Math.PI) / 180;
-            double y = Math.Sin(agra) * t;
+            double arad = (a * Math.PI) / 180;
+            double y = Math.Sin(arad) * t;
             Console.WriteLine("El cateto y es de " + y + " unidades");
             //encontrar cateto z
-            double z = Math.Cos(agra) * t;
+            double z = Math.Cos(arad) * t;
             Console.WriteLine("El cateto z es de " + z + " unidades");
             //hallando angulo c
             double c = Math.Asin(z / t);
