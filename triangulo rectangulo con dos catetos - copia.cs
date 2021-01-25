d01dac9ba82d72801a7182e51ab6ef3b286b3205
Jhon Sebastian Suarez Perez <77938466+jhon-ely@users.noreply.github.com>
2021-01-24 19:05:30 -0500

    Add files via upload

---
+++ triangulo rectangulo con dos catetos - copia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace triangulo_rectamgulo_con_dos_catetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escriba cateto y");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("escriba cateto z");
+            double z = double.Parse(Console.ReadLine());
+            //hallar t osea la hipo10USA
+            double t = Math.Sqrt(Math.Pow(z,2)+ Math.Pow(y, 2));
+            Console.WriteLine("La hipotenusa t es de " + t);
+            // hallar angulo a
+            double angarad = Math.Asin((y/t));
+            Console.WriteLine("el angulo a es de "+ angarad + " radianes");
+            double angagra = (angarad*180)/Math.PI;
+            Console.WriteLine("el angulo a es de " + angagra + " grados");
+            // hallar angulo c
+            double angcrad = Math.Asin((z/ t));
+            Console.WriteLine("el angulo c es de " + angcrad + " radianes");
+            double angcgra = (angcrad * 180) / Math.PI;
+            Console.WriteLine("el angulo c es de " + angcgra + " grados");
+
+
+        }
+    }
+}
