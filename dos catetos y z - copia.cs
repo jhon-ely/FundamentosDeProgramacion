244a4076a7904fa0fd0812fdfc6560d54833f377
Jhon Sebastian Suarez Perez <77938466+jhon-ely@users.noreply.github.com>
2021-01-24 21:38:38 -0500

    Add files via upload

---
+++ dos catetos y z - copia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dos_catetos_y_z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escriba cateto y");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("escriba cateto z");
+            double z = double.Parse(Console.ReadLine());
+            //hallar t osea la hipo10USA
+            double t = Math.Sqrt(Math.Pow(z, 2) + Math.Pow(y, 2));
+            Console.WriteLine("La hipotenusa t es de " + t);
+            // hallar angulo a
+            double angarad = Math.Asin((y / t));
+            Console.WriteLine("el angulo a es de " + angarad + " radianes");
+            double angagra = (angarad * 180) / Math.PI;
+            Console.WriteLine("el angulo a es de " + angagra + " grados");
+            // hallar angulo c
+            double angcrad = Math.Asin((z / t));
+            Console.WriteLine("el angulo c es de " + angcrad + " radianes");
+            double angcgra = (angcrad * 180) / Math.PI;
+            Console.WriteLine("el angulo c es de " + angcgra + " grados");
+        }
+    }
+}
---
+++ hipotenusa t angulo a - copia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hipotenusa_t_y_angulo_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Este programa sirve para hallar los catetos y el angulo faltante, con un angulo (no el recto) y la hipotenusa
+            //ingreso de datos
+            Console.WriteLine("escriba hipotenusa t");
+            double t = double.Parse(Console.ReadLine());
+            Console.WriteLine("escriba el angulo a");
+            double a = double.Parse(Console.ReadLine());
+            //encontrar cateto y
+            double agra = (a * Math.PI) / 180;
+            double y = Math.Sin(agra) * t;
+            Console.WriteLine("El cateto y es de " + y + " unidades");
+            //encontrar cateto z
+            double z = Math.Cos(agra) * t;
+            Console.WriteLine("El cateto z es de " + z + " unidades");
+            //hallando angulo c
+            double c = Math.Asin(z / t);
+            Console.WriteLine("El angulo c es de " + c + " radianes");
+            double cgra = (c * 180) / Math.PI;
+            Console.WriteLine("El angulo c es de " + cgra + " grados");
+        }
+    }
+}
